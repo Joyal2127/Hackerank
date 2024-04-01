095b635331a29604c2c0a5257ae0e6d9b14201c0
Joyal Puthur <150194916+Joyal2127@users.noreply.github.com>
2024-04-01 19:34:33 -0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,16 +1,21 @@
 def is_leap(year):
     leap = False
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                 leap = True
+    
+    if 1900 <= year <= 100000:
+        if year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    leap = True20
+                else:
+                    leap = False
             else:
-                leap = False
+                leap = True
         else:
             leap = False
     else:
-        leap = False
-                
+        print("not in range")
+    
     return leap
 
 year = int(input())
+print(is_leap(year))
